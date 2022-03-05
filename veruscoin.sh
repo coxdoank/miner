25c3de04b821c8f507866fe3db3b40e1d70ca70c
coxdoank <harryhandoko@rocketmail.com>
2022-03-05 17:50:38 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,4 +1,4 @@
 #veruscoin
 sudo apt get
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVfAiYr4Pip8nm8b3CGGW8cgqRFfJ6XySY.hha_01 -p x --cpu 4
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVfAiYr4Pip8nm8b3CGGW8cgqRFfJ6XySY.hha_01 -p x --cpu 8
